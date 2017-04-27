e85cb67eed36b07ee9fe12ae6b83a6f5ce5c7d1f
dppedersen <pedersen.duncan@gmail.com>
2017-04-27 19:29:12 -0400

    Move Services Folder, Set Up index.html file

---
+++ client/app/services/services.js
@@ -0,0 +1,17 @@
+angular.module('app.services', [])
+
+.factory('Parks', function ($http) {
+
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: 'str'//TO DO
+    }).then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  return {
+    getAll: getAll
+  };
+});
