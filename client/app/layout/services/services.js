d124d47988d1fd55eeadd60269e2db4e0076a6f1
dppedersen <pedersen.duncan@gmail.com>
2017-04-27 18:56:05 -0400

    Build Parks Controller and Parks Factory

--- client/app/layout/services/services.js
+++ client/app/layout/services/services.js
@@ -1,5 +1,17 @@
 angular.module('app.services', [])
 
-.factory('Parks', function($http) {
-  
-});
+.factory('Parks', function ($http) {
+
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: 'str'//TO DO
+    }).then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  return {
+    getAll: getAll
+  };
+})
--- client/app/parks/parks.js
+++ client/app/parks/parks.js
@@ -1,7 +1,18 @@
 angular.module('app.parks', [])
 
-.controller('ParksController', function($scope) {
+.controller('ParksController', function ($scope, Parks) {
 
+  $scope.data = {};
 
+  var initializeParks = function () {
+    Parks.getAll()
+      .then(function (parks) {
+        $scope.data.parks = parks;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
+  initializeParks();
 });
