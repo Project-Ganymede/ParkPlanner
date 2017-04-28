912d8607d3e368cd52f91fa0f9e391bbb8fce352
Giancarlo Guzman Oki <giancarloguzo@gmail.com>
2017-04-28 16:30:03 -0400

    Adding Client Routing and Rides Basic view

--- client/app/parks/parks.js
+++ client/app/parks/parks.js
@@ -2,17 +2,4 @@ angular.module('app.parks', [])
 
 .controller('ParksController', function ($scope, Parks) {
 
-  $scope.data = {};
-
-  var initializeParks = function () {
-    Parks.getAll()
-      .then(function (parks) {
-        $scope.data.parks = parks;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
-
-  initializeParks();
 });
