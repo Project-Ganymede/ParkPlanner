bc5a81c74217c88496878a56f9f317f7acc5a9c1
dppedersen <pedersen.duncan@gmail.com>
2017-05-03 15:15:22 -0400

    Update rides.js to Include Code to Initialize Sample Data for Rides and Plots

--- client/app/rides/rides.js
+++ client/app/rides/rides.js
@@ -8,5 +8,14 @@ angular.module('app.rides', [])
   };
   $scope.removeRideFromList = function (indexToRemove) {
     $scope.rideList.splice(indexToRemove,1);
+  };
+
+  //THE FOLLOWING CODE IS TO INITIALIZE RIDES AND PLOT
+  //DATA FOR THE PURPOSE OF TESTING THE RIDES-LIST-VIEW
+  $scope.rideQueue = $scope.rides;
+  $scope.data = [10, 20, 10, 20, 10, 20, 10];
+  $scope.labels = ['8am', '10am', '12pm', '2pm', '4pm', '6pm', '8pm'];
+  for (var i = 0; i < $scope.rideList.length; i++) {
+    $scope.rideList[i].data = [65, 59, 80, 81, 56, 55, 40];
   }
 });
