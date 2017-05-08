097fed276b3611b956650ba0c25609c8543658f6
Alex Biczak <biczak@Alexs-MacBook-Pro.local>
2017-05-08 14:16:58 -0400

    Fixed leftover merge conflict

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 require('./../workers/WaitTime');
->>>>>>> origin/dev
 
 // Sets the port to either the Process Environment's or 3000.
 let port = process.env.PORT || 3000;
