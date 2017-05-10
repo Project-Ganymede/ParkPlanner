386284eabf2ace663a4a954676917fe00d31c9ad
Leo Gomez <leogomez@gmail.com>
2017-05-10 12:04:08 -0400

    Write date and time to rideWaitTime table

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -158,7 +158,7 @@ let helpers = {
                 date: moment(waitTimeObj.lastUpdate).format('L'),
                 // add hour
                 hour: moment(waitTimeObj.lastUpdate).format('LT'),
-             });
+             }).save();
           //   .catch(err => console.log(err));
           // })
           // .catch(err => console.log(err));
@@ -230,7 +230,14 @@ let helpers = {
             .then(apiRidesArr => {
               apiRidesArr.forEach(apiRideObj=> {
                 helper.createNewRide(apiRideObj, parkEntry);
-              });
+              })
+              .then(() => {
+                console.log('in the .then');
+              })
+              .catch(() => {
+                console.log('in the .catch')
+              })
+              ;
             });
         });
       }
